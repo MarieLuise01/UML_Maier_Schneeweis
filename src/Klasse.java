7d9362acf95221d6ed30b58d38f37b0caafe57ef
snow2000 <flo.schneeweis@gmail.com>
2019-01-19 21:17:45 +0100

    fach/raum/klasse hinzugefügt

---
+++ src/Klasse.java
@@ -0,0 +1,25 @@
+
+public class Klasse {
+	
+	
+	protected
+		
+		String bezeichnung = "";
+		int schulstufe;
+		
+		
+		
+		public
+		 String getBezeichnung() {
+			return bezeichnung;
+		}
+		
+		int getSchulstufe() {
+			return schulstufe;
+		}
+	
+	
+		
+
+
+}
---
+++ src/Raum.java
@@ -0,0 +1,9 @@
+
+public class Raum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
