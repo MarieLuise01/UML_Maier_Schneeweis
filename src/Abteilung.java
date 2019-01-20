ab23cc0f04e359e9217835a3d3cc157bca05b412
MarieLuise01 <mlm2001@outlook.de>
2019-01-20 17:40:10 +0100

    Abteilung

--- src/Abteilung.java
+++ src/Abteilung.java
@@ -9,11 +9,26 @@ public class Abteilung {
 			String kuerzel;
 			String name;
 			Lehrer av;
-			
+		protected Schule Schule;
 		public HashSet<Schüler> Abtschülerliste = new HashSet<>();
 			
 		HashSet<Klasse> klasse = new HashSet<>();
-			
+		HashSet<Lehrer> Lehrerliste = new HashSet<>();
+		
+		 
+		Abteilung(String n, String k) {
+			name = n;
+			kuerzel = k;
+		}
+		
+		String getName() {
+			return name;
+		}
+		
+		String getKuerzel() {
+			return kuerzel;
+		}
+		
 		HashSet<Schüler> getSchüler(){
 			for(Klasse kl : klasse) {
 				Abtschülerliste.addAll(kl.Klschülerliste);
@@ -22,9 +37,27 @@ public class Abteilung {
 			
 			return Abtschülerliste;
 		}
-		 
-		Abteilung(String n, String k) {
-			name = n;
-			kuerzel = k;
+		
+		boolean addLehrer(Lehrer l) {
+			return Lehrerliste.add(l);
 		}
+		
+		
+		boolean setAbteilungsvorstand(Lehrer l)
+		{
+			if(Schule.direktor == l) {
+				return false;
+			}
+			this.av = l;
+			return true;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
